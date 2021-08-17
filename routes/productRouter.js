08e515ffce8da78a17e93466995f9f5650f89bd0
Loc Nguyen <loc.nguyen@aencoin.com>
2021-08-17 23:54:20 +0700

    Integrate getting data

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,26 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('category');
+router.get('/', async(req, res, next) => {
+    let categoryController = require('../controllers/categoryController');
+    let productController = require('../controllers/productController');
+    let brandController = require('../controllers/brandController');
+    let colorController = require('../controllers/colorController');
+
+    try {
+        const categories = await categoryController.getAll();
+        const brands = await brandController.getAll();
+        const colors = await colorController.getAll();
+        const products = await productController.getAll();
+
+        res.locals.categories = categories;
+        res.locals.brands = brands;
+        res.locals.colors = colors;
+        res.locals.products = products;
+        res.render('category');
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/:id', (req, res) => {
