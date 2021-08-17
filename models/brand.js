10415a66324003a79bcbae5cff476ddf5c36376c
Loc Nguyen <loc.nguyen@aencoin.com>
2021-08-17 22:58:59 +0700

    Integrate db

---
+++ models/brand.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Brand extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Brand.hasMany(models.Product, {foreignKey: 'brandId'});
+    }
+  };
+  Brand.init({
+    name: DataTypes.STRING,
+    summary: DataTypes.TEXT,
+    imagepath: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Brand',
+  });
+  return Brand;
+};
