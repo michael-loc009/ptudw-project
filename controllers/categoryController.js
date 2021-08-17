d9ae2941b34eb30970ed5721ce85695817ddd51c
Loc Nguyen <loc.nguyen@aencoin.com>
2021-08-17 23:53:59 +0700

    Update controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,12 +5,14 @@ let Category = models.Category;
 controller.getAll = () => {
     return new Promise((resolve, reject) => {
         Category.findAll({
-                attributes: ['id', 'name', 'imagepath', 'summary']
+                attributes: ['id', 'name', 'imagepath', 'summary'],
+                include: [{model: models.Product}]
             })
             .then(data => {
                 const categories = data.map((item) => { 
                     let values = item.dataValues;
-                    return { id: values.id, name: values.name, imagepath: values.imagepath, summary: values.summary } });
+                    let products = item.dataValues.Products;
+                    return { id: values.id, name: values.name, imagepath: values.imagepath, summary: values.summary, products: products} });
                 resolve(categories);
             })
             .catch(error => reject(new Error(error)));
--- controllers/productController.js
+++ controllers/productController.js
@@ -25,4 +25,22 @@ controller.getTrendingProducts = () => {
 };
 
 
+controller.getAll = () => {
+    return new Promise((resolve, reject) => {
+        Product.findAll({
+                include: [{ model: models.Category }],
+                attributes: ['id', 'name', 'imagepath', 'price']
+            })
+            .then(data => {
+                const trendingProducts = data.map((item) => {
+                    let values = item.dataValues;
+                    let category = item.Category.dataValues;
+                    return { id: values.id, name: values.name, imagepath: values.imagepath, price: values.price, category: category }
+                });
+                resolve(trendingProducts);
+            })
+            .catch(error => reject(new Error(error)));
+    });
+};
+
 module.exports = controller;
